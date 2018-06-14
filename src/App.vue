78b94b4d8113d149c835bc1c7099b71e0d2c5676
Ilich Arredondo <ilichh1@hotmail.com>
2018-06-13 19:34:46 -0500

    Created helper classes and added fade transition

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,28 @@ export default {
 <style lang="scss">
 @import '~bulma/bulma';
 
+// Transitions and animations
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 250ms ease;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-to, .fade-leave {
+  opacity: 1;
+}
+
+// Helper classes
+.c-pointer {
+  cursor: pointer !important;
+}
+
+.d-inline-block {
+  display: inline-block !important;
+}
+
 .w-100 {
   width: 100% !important;
 }
