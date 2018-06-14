503cbf9d4e788738c4aaa99b0ce629a184343915
Ilich Arredondo <ilichh1@hotmail.com>
2018-06-13 19:34:59 -0500

    Added Oscar chanfes

--- src/components/CountrySelection.vue
+++ src/components/CountrySelection.vue
@@ -1,77 +1,126 @@
 <template>
   <section id="woonkly-country-selection" class="section">
     <div class="container">
-      <div class="columns">
-        <form class="column is-one-third-tablet is-8-desktop" @submit.prevent="submitForm">
-          <div class="field">
-            <label class="label">Bienvenido!</label>
-            <div class="control">
-              <input class="input" type="text" placeholder="Text input" :value="userName" disabled>
+
+      <transition name="shrink">
+        <div v-show="!termsAndConditions" class="level" style="transition-delay: 600ms">
+          <div class="columns">
+            <div id="terms-and-conditions" class="column column is-one-third-tablet is-8-desktop">
+              <h1 class="title is-3">Términos y condiciones</h1>
+              <div class="wrapper">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut imperdiet in lorem quis aliquet. Vestibulum finibus lectus sed nibh lacinia dapibus. Duis urna est, maximus dignissim massa quis, cursus feugiat quam. Phasellus eu lorem sollicitudin, pellentesque arcu in, consequat mi. Integer tincidunt lectus augue, vitae vestibulum est scelerisque id. Praesent convallis aliquet cursus. Morbi consectetur mi eu imperdiet sollicitudin. Nam feugiat augue ac laoreet feugiat. Etiam venenatis velit sapien, in euismod risus vehicula at. Sed condimentum ultrices sem in tincidunt. Cras vel nisl sit amet quam viverra feugiat eget eget tellus. Curabitur eget eleifend nibh. Sed feugiat risus non ipsum malesuada, a malesuada tortor mollis. Aliquam vitae leo tincidunt, egestas mauris in, vestibulum dui. In aliquam lacus in mi hendrerit, venenatis pharetra ligula feugiat.
+
+                  Integer faucibus risus id posuere facilisis. Cras efficitur aliquam ante, nec tempus lacus bibendum a. Vivamus ultricies id augue non gravida. Maecenas risus elit, sagittis elementum metus et, viverra placerat sem. Ut eu ipsum in dolor vestibulum sagittis. Proin mattis, quam non finibus lobortis, turpis justo convallis nunc, eget fringilla ipsum lorem vitae mi. In hac habitasse platea dictumst. Praesent sed volutpat lectus, eget commodo felis. Vivamus ligula mi, congue eu sagittis quis, ultricies eu sapien. Nullam aliquet nulla rhoncus mauris aliquet, a pellentesque urna congue.
+
+                  In eu condimentum ipsum. Fusce iaculis, ex eu placerat euismod, sapien risus eleifend nibh, quis efficitur velit libero ut eros. Etiam commodo laoreet ligula, non mollis neque malesuada at. Sed vehicula urna vitae eleifend efficitur. Cras efficitur nunc sem. Nulla facilisi. Donec vestibulum arcu a odio fermentum varius. Sed id justo felis. Suspendisse rhoncus convallis felis, vel egestas quam volutpat et. Vivamus id tincidunt sapien. Sed condimentum odio eget diam suscipit, ac luctus diam condimentum. Aenean aliquam lorem at felis pharetra egestas. Integer eu mi venenatis, vehicula diam vitae, condimentum augue. Maecenas ornare dapibus urna rutrum ornare.
+
+                  Curabitur aliquam ipsum vitae nisi mattis, et varius arcu posuere. Nullam vitae hendrerit augue. In dignissim molestie est at tempor. In tempus finibus metus vel convallis. Aliquam sapien lacus, placerat vitae congue non, vulputate vel neque. Nullam odio ipsum, gravida quis consectetur non, volutpat id nulla. Vivamus in placerat nisi. Cras quis congue sapien, at porttitor libero. Proin vehicula tortor nisi, eget luctus neque maximus sed. Nam bibendum diam ipsum, vitae pellentesque sapien blandit eget. Fusce id cursus leo. Suspendisse potenti. Pellentesque id mollis lacus. Proin sagittis sapien id viverra fermentum. Mauris dui augue, imperdiet ut sagittis id, luctus vel ipsum.
+
+                  Sed mattis suscipit urna vel imperdiet. Suspendisse sit amet facilisis ipsum, sed sodales ligula. Donec nec sagittis augue. Proin dui diam, consequat tristique sollicitudin eget, suscipit vitae massa. Proin finibus ac turpis quis placerat. Curabitur ac nulla feugiat, faucibus nisi in, sagittis massa. Ut malesuada sollicitudin mi sit amet vulputate. Donec auctor nisi sed ipsum efficitur dapibus. In sed erat nec odio auctor maximus accumsan in mauris. Duis sodales ante sit amet interdum efficitur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec pellentesque lorem et sollicitudin mollis.
+
+                  Pellentesque in neque iaculis, interdum eros eget, vehicula metus. Sed finibus, lacus sit amet lobortis varius, lectus purus rhoncus odio, quis iaculis justo mauris et tellus. Mauris at fermentum dui. Nullam purus magna, volutpat eget consequat in, fringilla eget ligula. Integer eu porta libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat ac orci scelerisque mattis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+
+                  Proin congue dui in urna dapibus blandit. Ut sodales vehicula sem, ut sodales velit interdum faucibus. Nullam ullamcorper laoreet neque in varius. Cras viverra faucibus cursus. Nullam et felis finibus, molestie nibh eu, vehicula dolor. Aenean gravida velit non placerat laoreet. Quisque lorem nisi, vulputate sit amet viverra a, vehicula id velit. Duis tempus mollis libero, eu placerat nibh pellentesque vitae. Sed pharetra purus mi, eget eleifend nisi tempor vel. Nulla facilisi. Phasellus lacinia aliquet nibh, eu interdum dolor euismod id. Ut tellus risus, gravida non imperdiet eu, blandit a risus. Donec non orci non neque maximus vehicula in sit amet mauris. Duis non elit non nunc pulvinar convallis. Morbi ac quam luctus, suscipit eros sed, semper orci.
+
+                  Pellentesque maximus massa ut porttitor luctus. Duis vel libero nisi. Nulla ut metus mauris. Nullam ultrices commodo sapien, ut tempor magna consectetur vel. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent mattis in nibh sed ullamcorper. Cras mollis, quam ac imperdiet viverra, nunc elit suscipit dolor, eget ultrices ante urna nec sem. Phasellus dictum, urna ac mollis tempus, felis enim condimentum nulla, eget consectetur sapien quam id est. Maecenas vitae viverra purus. Morbi viverra tincidunt magna, non convallis orci malesuada non.
+
+                  Donec id nisi vestibulum, pretium metus sed, hendrerit enim. Nunc pulvinar laoreet dolor, eu volutpat nisl finibus in. Sed venenatis quam at fermentum facilisis. Suspendisse fermentum sapien ac imperdiet aliquam. Integer volutpat arcu in nisi pretium, ac porttitor justo convallis. Aenean interdum felis ut gravida placerat. Praesent semper vitae ex ac commodo. Etiam viverra et velit id semper. Donec varius cursus rutrum. Integer accumsan, dolor non egestas pulvinar, libero purus pellentesque ligula, sed maximus ante urna vitae erat. Donec sit amet finibus lorem.
+
+                  Phasellus ac neque ultricies, lobortis nulla et, facilisis enim. Phasellus dictum purus eget semper tincidunt. Ut rhoncus ullamcorper mollis. Ut facilisis lorem luctus enim lobortis, ut condimentum metus egestas. Integer ut eros id sem tempus ultricies vitae eget ante. Phasellus efficitur gravida massa, eu lobortis sapien gravida ac. Quisque gravida dui purus, a feugiat massa malesuada eget. Donec rhoncus laoreet rutrum.
+
+                  Proin in urna eget nibh fringilla pellentesque. Phasellus sollicitudin accumsan lobortis. Sed interdum, quam at varius accumsan, urna orci blandit lorem, a venenatis quam orci vel justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin eget libero vitae eros congue posuere. Sed sagittis lorem at mattis maximus. Mauris varius efficitur metus.
+
+                  Proin non nisi elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas sed varius quam. In mi metus, accumsan et rhoncus vitae, cursus id orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum est mi, venenatis quis pharetra sed, elementum vitae justo. Sed rutrum quam non commodo lacinia. Cras at mauris at libero vehicula gravida vel quis libero. Quisque et mollis lacus. Sed viverra rutrum interdum. Nam fringilla tellus quis luctus ullamcorper. Praesent neque nulla, venenatis et lacus non, aliquet varius nulla. Sed sed lobortis leo. Aliquam iaculis ullamcorper mi ac consequat. Nunc vestibulum hendrerit mi vel dictum.
+
+                  Praesent a purus ut dolor cursus eleifend at finibus enim. Nam auctor eleifend ex, quis sollicitudin nibh. Phasellus non elementum massa. Aliquam condimentum felis et eros suscipit commodo. Fusce luctus diam felis. Morbi ultrices odio nec tellus iaculis, eget iaculis nulla laoreet. Donec et purus gravida, sollicitudin arcu ac, maximus libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur cursus lorem eu porttitor. Ut euismod placerat metus, vel laoreet eros consectetur sed.
+
+                  Duis semper vehicula neque id pretium. Integer fringilla placerat sollicitudin. Nam vehicula ac nunc ut auctor. Sed vestibulum sagittis sapien, quis interdum diam suscipit eu. Vestibulum ante libero, iaculis sit amet tincidunt a, pulvinar at lectus. Sed condimentum, tellus id vestibulum aliquet, augue nisl viverra enim, non fringilla lorem leo ut augue. Etiam ut pharetra risus. Morbi finibus lectus rhoncus varius varius. Quisque vitae dictum massa. Aliquam suscipit justo ipsum, at ullamcorper nibh hendrerit vel. Sed tincidunt dui a mi pharetra vulputate. Mauris commodo ac nunc ac faucibus. Praesent congue id quam sit amet bibendum. Pellentesque bibendum, odio et ultricies cursus, velit ipsum tincidunt augue, et viverra turpis leo nec enim.
+
+                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas lobortis, arcu a consequat auctor, orci elit consequat orci, ut mollis urna nulla at mauris. Phasellus tincidunt dui vitae ipsum suscipit ornare. Pellentesque finibus mi id arcu vulputate, eu tempus ipsum tempus. Quisque auctor eleifend dapibus. Duis sed lorem porttitor, accumsan nulla nec, ultricies est. Mauris blandit blandit eleifend. Proin convallis ante ut auctor laoreet. Morbi non cursus dolor. Quisque vehicula gravida orci. Morbi eget egestas ante. Phasellus bibendum laoreet ipsum at aliquam. Etiam accumsan, diam sit amet facilisis vehicula, nibh purus accumsan lorem, a auctor nisi nibh at enim. Praesent ante purus, accumsan sit amet malesuada sodales, semper eget sem. Pellentesque eu ex volutpat, aliquet tellus ac, venenatis purus.
+
+                  Maecenas ut ultricies tellus. Morbi gravida orci et dolor porta mattis. Nunc auctor risus efficitur, interdum est vel, semper magna. Sed vel nibh arcu. Nunc vel libero dui. Integer est erat, pellentesque sit amet pellentesque sit amet, tempor ut nunc. Cras at euismod magna. Duis at ante accumsan, sagittis tortor ac, ornare massa. Maecenas hendrerit sapien arcu, eget maximus eros tincidunt id.
+                </p>
+              </div>
+              <w-checkbox class="woonkly-checkbox" v-model="termsAndConditions" label="Acepto los términos y condiciones"/>
             </div>
           </div>
+        </div>
+      </transition>
 
-          <div class="field">
-            <label class="label">País #1</label>
-            <div class="control">
-              <div class="select">
-                <select v-model="selectedCountries[0]">
-                  <option value="null" disabled selected>Seleccione...</option>
-                  <option v-for="c in countriesArray" :disabled="selectedCountries.includes(c)">{{c}}</option>
-                </select>
+      <transition name="shrink">
+        <div v-show="termsAndConditions" class="columns" style="transition-delay: 1420ms">
+          <form class="column is-one-third-tablet is-8-desktop" @submit.prevent="submitForm">
+            <div class="field">
+              <label class="label">Bienvenido!</label>
+              <div class="control">
+                <input class="input" type="text" placeholder="Text input" :value="userName" disabled>
               </div>
             </div>
-          </div>
 
-          <div class="field">
-            <label class="label">País #2</label>
-            <div class="control">
-              <div class="select">
-                <select v-model="selectedCountries[1]">
-                  <option value="null" disabled selected>Seleccione...</option>
-                  <option v-for="c in countriesArray" :disabled="selectedCountries.includes(c)">{{c}}</option>
-                </select>
+            <div class="field">
+              <div class="control">
+                <div class="select">
+                  <select v-model="selectedCountries[0]">
+                    <option value="null" disabled selected>Primer lugar</option>
+                    <option v-for="c in countriesArray" :disabled="selectedCountries.includes(c)">{{c}}</option>
+                  </select>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div class="field">
-            <label class="label">País #3</label>
-            <div class="control">
-              <div class="select">
-                <select v-model="selectedCountries[2]">
-                  <option value="null" disabled selected>Seleccione...</option>
-                  <option v-for="c in countriesArray" :disabled="selectedCountries.includes(c)">{{c}}</option>
-                </select>
+            <div class="field">
+              <div class="control">
+                <div class="select">
+                  <select v-model="selectedCountries[1]">
+                    <option value="null" disabled selected>Segundo lugar</option>
+                    <option v-for="c in countriesArray" :disabled="selectedCountries.includes(c)">{{c}}</option>
+                  </select>
+                </div>
               </div>
             </div>
-          </div>
 
-          <!-- <div class="field">
-            <label class="label">Message</label>
-            <div class="control">
-              <textarea class="textarea" placeholder="Textarea"></textarea>
+            <div class="field">
+              <div class="control">
+                <div class="select">
+                  <select v-model="selectedCountries[2]">
+                    <option value="null" disabled selected>Tercer lugar</option>
+                    <option v-for="c in countriesArray" :disabled="selectedCountries.includes(c)">{{c}}</option>
+                  </select>
+                </div>
+              </div>
             </div>
-          </div> -->
 
-          <div class="field">
-            <div class="control">
-              <label class="checkbox">
-                <input type="checkbox" v-model="termsAndConditions">
-                Acepto los <a href="#">términos y condiciones.</a>
-              </label>
+            <!-- <div class="field">
+              <label class="label">Message</label>
+              <div class="control">
+                <textarea class="textarea" placeholder="Textarea"></textarea>
+              </div>
+            </div> -->
+
+            <!-- <div class="field">
+              <div class="control">
+                <label class="checkbox">
+                  <input type="checkbox" v-model="termsAndConditions">
+                  Acepto los <a href="#">términos y condiciones.</a>
+                </label>
+              </div>
+            </div> -->
+
+            <div class="buttons">
+              <button class="button w-100 is-link is-marginless is-block">Enviar</button>
             </div>
-          </div>
+          </form>
+        </div>
+      </transition>
 
-          <div class="buttons">
-            <button class="button w-100 is-link is-marginless is-block">Enviar</button>
-          </div>
-        </form>
-      </div>
     </div>
   </section>
 </template>
 
 <script>
+import wCheckbox from '@/components/unit/WoonklyCheckbox'
+
 export default {
   name: 'CountrySelection',
   data () {
@@ -94,12 +143,35 @@ export default {
         // TODO: Notify the user that something is incomplete
       }
     }
+  },
+  components: {
+    wCheckbox
   }
 }
 </script>
 
 <style lang="scss">
 #woonkly-country-selection {
+
+  #terms-and-conditions {
+    .wrapper {
+      border: 1.5px solid #000;
+      border-radius: 0.4em;
+      & > p {
+        padding: 0.5em;
+        overflow: auto;
+        max-height: 250px;
+        text-align: justify;
+      }
+    }
+    .woonkly-checkbox {
+      margin-top: 1.5em;
+    }
+  }
+
+  .columns > form {
+    margin: auto;
+  }
   .control {
     .select, .select > select {
       width: 100%;
@@ -107,14 +179,24 @@ export default {
   }
 }
 
+.shrink-enter-active, .shrink-leave-active {
+  transition: transform 800ms ease;
+  transform-origin: top center;
+}
+
+.shrink-enter, .shrink-leave-to {
+  transform: scaleY(0);
+}
+
+.shrink-enter-to, .shrink-leave {
+  transform: scaleY(1);
+}
+
 // mobile-only styles
 @media only screen and (max-width: 1024px) {
   #woonkly-country-selection {
     min-height: calc(100vh - 52px);
     box-sizing: border-box;
-    .columns > form {
-      margin: auto;
-    }
   }
 }
 </style>
