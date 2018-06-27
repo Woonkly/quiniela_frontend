1fd677f808fc53a5e51d7cf9d02b46ba2a819877
Ilich Arredondo <ilichh1@hotmail.com>
2018-06-26 21:49:18 -0500

    Changed logic to call 'finishedValidation' with true response

--- src/utils/web3Validator.js
+++ src/utils/web3Validator.js
@@ -3,35 +3,38 @@ export function web3PresentAndValidated (finishedValidation, setAccount) {
     finishedValidation(false)
     console.log('Please install metamask or use mist')
     return false
-  }
-
-  window.web3.eth.getAccounts((err, acc) => {
-    if (err !== null) {
-      console.error(err)
-      finishedValidation(false)
-    }
-    if (acc.length === 0) {
-      console.log('Please login with your ETH account')
-      finishedValidation(false)
-    } else {
-      setAccount(acc[0])
-    }
-  })
-
-  window.web3.version.getNetwork((err, netId) => {
-    if (err !== null) {
-      console.error(err)
-      finishedValidation(false)
-    }
-    switch (netId) {
-      case '1':
-        console.log('The user is on the mainet')
-        // TODO: Once in production set the validation in this point
-        break
-      default:
-        console.log('User not in mainet', `Network ID: ${netId}`)
+  } else {
+    window.web3.eth.getAccounts((err, acc) => {
+      if (err !== null) {
+        console.error(err)
         finishedValidation(false)
-    }
-  })
+      }
+      if (acc.length === 0) {
+        console.log('Please login with your ETH account')
+        finishedValidation(false)
+      } else {
+        setAccount(acc[0])
+        window.web3.version.getNetwork((err, netId) => {
+          if (err !== null) {
+            console.error(err)
+            finishedValidation(false)
+          }
+          switch (netId) {
+            case '1':
+              console.log('The user is on the mainet')
+              // TODO: Once in production set the validation in this point
+              break
+            case '3':
+              console.log('ON ROPSTEN NETWORK')
+              finishedValidation(true)
+              break
+            default:
+              console.log('User not in mainet', `Network ID: ${netId}`)
+              finishedValidation(false)
+          }
+        })
+      }
+    })
+  }
   // End of the validation
 }
