b10c3fb7caab2856a2e6dbfdff2ba56e4cf2363d
Ilich Arredondo <ilichh1@hotmail.com>
2018-06-28 02:25:04 -0500

    Changed URL to copy when a new user is added

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_URL: '"http://localhost:8080"',
-  COUNTRIES_URL: '"http://localhost:8081/#/countries"'
+  COUNTRIES_URL: '"http://localhost:8081/#/gamble"'
 })
