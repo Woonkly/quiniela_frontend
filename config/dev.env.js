e0eaafb43cca9f699d7cd04fbecfd374b6ec1716
Ilich Arredondo <ilichh1@hotmail.com>
2018-06-28 21:17:20 -0500

    Added the env variable to validate the ETH network

--- config/dev.env.js
+++ config/dev.env.js
@@ -8,5 +8,6 @@ module.exports = merge(prodEnv, {
   BASE_URL: '"http://localhost:8080"',
   COUNTRIES_URL: '"http://localhost:8081/#/gamble"',
   CONTRACT_ADDRESS: '"0x987fD0d0cCeC5E37E3f050E9B2F25f0b3c4e042F"',
-  ETHERSCAN_URL: '"https://ropsten.etherscan.io/tx"'
+  ETHERSCAN_URL: '"https://ropsten.etherscan.io/tx"',
+  NETWORK_ID: '"3"' // 3 -> ROPSTEN network | 1 -> Mainet
 })
