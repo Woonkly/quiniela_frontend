0bb5b47d1be366814f33f832687def04b9e1a069
Ilich Arredondo <ilichh1@hotmail.com>
2018-06-29 13:40:31 -0500

    Fixed feedback messages

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,11 +1,10 @@
 'use strict'
 
-// TODO: Change this to true "production" values
-module.exports = merge(prodEnv, {
+module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL: '"http://localhost:8080"',
-  COUNTRIES_URL: '"http://localhost:8081/#/gamble"',
+  BASE_URL: '"http://ec2-18-219-245-172.us-east-2.compute.amazonaws.com"',
+  COUNTRIES_URL: '"http://boveda.woonkly.com/#/gamble"',
   CONTRACT_ADDRESS: '"0x509cCE0945DE322Eb7eb8bFB929bC0713F0576775"',
   ETHERSCAN_URL: '"https://ropsten.etherscan.io/tx"',
   NETWORK_ID: '"3"' // 3 -> ROPSTEN network | 1 -> Mainet
-})
+}
