90eb042ce4c77235ac64ffc9cfacb4e17b33ddca
Ilich Arredondo <ilichh1@hotmail.com>
2018-06-29 02:10:31 -0500

    Added preparations for production deploy

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,7 +2,6 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
-// TODO: Change ehterscan url and contract address when production ready
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_URL: '"http://localhost:8080"',
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,11 @@
 'use strict'
-module.exports = {
-  NODE_ENV: '"production"'
-}
+
+// TODO: Change this to true "production" values
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"production"',
+  BASE_URL: '"http://localhost:8080"',
+  COUNTRIES_URL: '"http://localhost:8081/#/gamble"',
+  CONTRACT_ADDRESS: '"0x509cCE0945DE322Eb7eb8bFB929bC0713F0576775"',
+  ETHERSCAN_URL: '"https://ropsten.etherscan.io/tx"',
+  NETWORK_ID: '"3"' // 3 -> ROPSTEN network | 1 -> Mainet
+})
