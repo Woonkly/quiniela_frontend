4362d53fe643b0544dc2722c3dc41d99f911f342
Ilich Arredondo <ilichh1@hotmail.com>
2018-07-02 14:49:46 -0500

    Changed env variables

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,7 +6,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_URL: '"http://localhost:8080"',
   COUNTRIES_URL: '"http://localhost:8081/#/gamble"',
-  CONTRACT_ADDRESS: '"0x6B812248B6Bd5E1bb31543219883D298a8C591a4"',
+  CONTRACT_ADDRESS: '"0x509cCE0945DE322Eb7eb8bFB929bC0713F057677"',
   ETHERSCAN_URL: '"https://ropsten.etherscan.io/tx"',
   NETWORK_ID: '"3"' // 3 -> ROPSTEN network | 1 -> Mainet
 })
