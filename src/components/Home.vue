7d1378d1169add16af7f38521dbbc3381c1bc3fa
Ilich Arredondo <ilichh1@hotmail.com>
2018-07-08 02:08:03 -0500

    Added players list

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,31 @@
   <section id="woonkly-home" class="section text-center">
     <span class="title is-2">Gracias por interesarte en la Woonkly Quiniela, pronto se anunciarán a los ganadores.</span>
     <hr>
+    <div class="players has-text-left" v-if="players.length !== 0">
+      <h4 class="title is-4">Jugadores actuales</h4>
+      <div class="colums">
+        <div class="column mx-auto has-text-centered">
+          <table class="table is-hoverable">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Primer lugar</th>
+                <th>Segundo lugar</th>
+                <th>Tercer lugar</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="p in players">
+                <td>{{p.name}}</td>
+                <td>{{p.firstPlace}}</td>
+                <td>{{p.secondPlace}}</td>
+                <td>{{p.thirdPlace}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
     <img style="margin-top:-20%;margin-bottom:-25%" @click="toggleClass" :class="{'spinning': woonklySpinning}" src="/static/woonkzalo-big.png" alt="Woonkzalo">
   </section>
 </template>
