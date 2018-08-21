4908a240af85bc477a5b457325a6a5fa79bccaca
Bryam Yhair Perez Diaz <bryamyhairperezdiaz@gmail.com>
2018-08-21 13:53:26 -0500

    Production Configuration.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,4 @@
+/*
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
@@ -7,14 +8,13 @@ module.exports = {
   ETHERSCAN_URL: '"https://ropsten.etherscan.io/tx"',
   NETWORK_ID: '"3"' // 3 -> ROPSTEN network | 1 -> Mainet
 }
-
-// 'use strict'
-//
-// module.exports = {
-//   NODE_ENV: '"production"',
-//   BASE_URL: '"http://ec2-18-219-245-172.us-east-2.compute.amazonaws.com"',
-//   COUNTRIES_URL: '"http://boveda.woonkly.com/#/gamble"',
-//   CONTRACT_ADDRESS: '""',
-//   ETHERSCAN_URL: '"https://etherscan.io/tx"',
-//   NETWORK_ID: '"1"' // 3 -> ROPSTEN network | 1 -> Mainet
-// }
+*/
+'use strict'
+ module.exports = {
+   NODE_ENV: '"production"',
+   BASE_URL: '"http://ec2-18-219-245-172.us-east-2.compute.amazonaws.com"',
+   COUNTRIES_URL: '"http://boveda.woonkly.com/#/gamble"',
+   CONTRACT_ADDRESS: '""',
+   ETHERSCAN_URL: '"https://etherscan.io/tx"',
+   NETWORK_ID: '"1"' // 3 -> ROPSTEN network | 1 -> Mainet
+ }
